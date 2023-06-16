badb5b275be50dfe31246067446be6740c458397
juan diego <132094846+tamara9559@users.noreply.github.com>
2023-06-16 11:01:23 -0500

    entrega final
    parcial 1

---
+++ a.js
@@ -0,0 +1,10 @@
+let tenis1 = ["adidas","talla:10","precio:10mil","año:2010","deportivo"]
+let tenis2 = ["puma","talla:15","precio:40mil","año:2020","casual"]
+let tenis3 = ["nike","talla:10","precio:50mil","año:2015","deportivo"]
+let tenis4 = ["croydon","talla:20","precio:60mil","año:2016","elegante"]
+
+
+console.log(tenis1)
+console.log(tenis2)
+console.log(tenis3)
+console.log(tenis4)
